c7a8dc2fc8279eef98c09b97a847eb443c4046e7
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-21 23:07:00 +0530

    Array sample

--- Five.java
+++ Five.java
@@ -2,5 +2,6 @@ public class Five{
     public static void main(String[] args) {
         String[] crypto = {"Bitcoin", "BinanceSmartChain", "Floki", "Sominia"};
         System.out.println(crypto[0]);
+        System.out.println(crypto[1]);
     }
 }
