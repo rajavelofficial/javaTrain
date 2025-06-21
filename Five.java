18d456ab2b46b09fa2652ad40d03753554e8e059
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-21 23:15:51 +0530

    using length function to find out no of Arrays

--- Five.java
+++ Five.java
@@ -1,7 +1,10 @@
 public class Five{
     public static void main(String[] args) {
-        String[] crypto = {"Bitcoin", "BinanceSmartChain", "Floki", "Sominia"};
+        String[] crypto = {"Bitcoin", "BinanceSmartChain", "Floki", "Sominia", "Doge"};
+        crypto[1] = "Ethereum";
+        System.out.println("Total Coins In Array :  " +crypto.length);
         System.out.println(crypto[0]);
         System.out.println(crypto[1]);
+        
     }
 }
