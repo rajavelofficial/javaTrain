1b7c1bc93593e5b8847b5271b7cac903dd3701a9
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-24 22:05:45 +0530

    in Multidimensional Array change the specific value

--- Multidimension.java
+++ Multidimension.java
@@ -1,7 +1,8 @@
 public class Multidimension {
     public static void main(String[] args) {
         String[][] cryptoCoin = {{"Bitcoin", "Ethereum", "Binance"}, {"Floki", "Doge", "Game7"}};
-        System.out.println(cryptoCoin [1][2]);
+        cryptoCoin[1][0] = "Beam";   //Floki meme token cheged to Beam token
+        System.out.println(cryptoCoin [1][0]);
     }
     
 }
