954459dc5a67f82977c694ea9ded2027499f2872
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-24 22:58:08 +0530

    Excute the set of function using method

---
+++ Method.java
@@ -0,0 +1,12 @@
+public class Method {
+    static void myMethod(){
+        System.out.println("Day 2");
+    }
+    static void meMethod(){
+        System.out.println("Day 1");
+    }
+    public static void main(String[] args) {
+        meMethod();
+        myMethod();
+    }
+}
