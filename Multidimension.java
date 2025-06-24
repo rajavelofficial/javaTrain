344a37a3aa4575bfc79eb256c51cc310de9d9aaf
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-24 22:02:24 +0530

    Syntax of Multidimensional Array

---
+++ Multidimension.java
@@ -0,0 +1,7 @@
+public class Multidimension {
+    public static void main(String[] args) {
+        String[][] cryptoCoin = {{"Bitcoin", "Ethereum", "Binance"}, {"Floki", "Doge", "Game7"}};
+        System.out.println(cryptoCoin [1][2]);
+    }
+    
+}
