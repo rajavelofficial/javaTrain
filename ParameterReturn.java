1d8e6165d5c9a9380f740993d8db1465d29b6625
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-25 21:47:53 +0530

    Developing Parameter return value

---
+++ ParameterReturn.java
@@ -0,0 +1,9 @@
+public class ParameterReturn {
+    static int myMethod( int num ){
+        return 5 + num ;
+    }
+    public static void main(String[] args) {
+        System.out.println(" Boy is actual age is: " + myMethod(4));
+    }
+    
+}
