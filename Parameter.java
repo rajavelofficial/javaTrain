ffe36c48dbc4638b08b79578672abb8cbd412820
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-25 21:20:54 +0530

    Developing Multiple Paremeter values passed

---
+++ Parameter.java
@@ -0,0 +1,11 @@
+public class Parameter {
+    static void myMethod(String name, int age){
+        System.out.println(name + " age is " + age);
+    }
+    public static void main(String[] args) {
+        myMethod( "Raja", 21);
+        myMethod("Arun", 21);
+        myMethod("Selva Raj", 28);
+    }
+    
+}
