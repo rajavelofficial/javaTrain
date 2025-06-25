b1190972f3d74a57084d90f7efb49c0ff012b26e
rajavelofficial <rajavelofficial2003@gmail.com>
2025-06-25 21:29:47 +0530

    Developing Multiple Parameter value passing and using If else

--- Parameter.java
+++ Parameter.java
@@ -2,10 +2,19 @@ public class Parameter {
     static void myMethod(String name, int age){
         System.out.println(name + " age is " + age);
     }
+    static void CheckAge( int age ){
+        if ( age < 18 ) {
+            System.out.println("Access Denied! - Because Your Miner");
+        }else{
+            System.out.println("Access Granted! ");
+        }
+    }
     public static void main(String[] args) {
         myMethod( "Raja", 21);
         myMethod("Arun", 21);
         myMethod("Selva Raj", 28);
+
+        CheckAge(20);
     }
     
 }
