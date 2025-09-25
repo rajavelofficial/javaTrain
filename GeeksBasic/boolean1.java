9634cd3af38a2382174849aab9a183a1e69a27c4
rajavelofficial <rajavelofficial2003@gmail.com>
2025-09-25 20:33:12 +0530

    one more time update the available files

---
+++ GeeksBasic/boolean1.java
@@ -0,0 +1,10 @@
+public class boolean1 {
+    public static void main(String[] args){
+        
+        boolean fun = true;
+        boolean taste = false;
+
+        System.err.println("Is java fun? " +fun );
+        System.err.println("Is Java is Taste? " +taste);
+    }  
+}
