49863ea470919467500b0bcf169e86012ca0d75a
rajavelofficial <rajavelofficial2003@gmail.com>
2025-10-02 21:35:52 +0530

    Interface, Array and Literal value Example update

---
+++ GeeksBasic/ArrayExample.java
@@ -0,0 +1,9 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5};
+        String[] fruit = {"Apple", "Leamon", "Banana", "BlackBerry"};
+
+        System.out.println("First Number is " +num[0]);
+        System.out.println(fruit[1]+ " is the Second fruit of list");
+    }
+}
---
+++ GeeksBasic/LiteralInteger.java
@@ -0,0 +1,18 @@
+
+public class LiteralInteger {
+    public static void main(String[] args) {
+        int Decimal = 2003;
+
+        int Octal = 0100;
+
+        int Binary = 0b1111 ;
+
+        int HexaDecimal = 0x2934f342 ;
+
+        System.out.println("Decimal Value : " +Decimal);
+        System.out.println("Octal Value : " +Octal);
+        System.out.println("Binary Value : " +Binary);
+        System.out.println("HexaDecimal Value : " +HexaDecimal);
+    }
+    
+}
