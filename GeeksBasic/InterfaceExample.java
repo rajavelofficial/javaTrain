98e231886c7fe3fc57e6cc8b37667ee7883e3675
rajavelofficial <rajavelofficial2003@gmail.com>
2025-10-02 08:00:43 +0530

    Implementing Java interface, Class and Method

---
+++ GeeksBasic/InterfaceExample.java
@@ -0,0 +1,16 @@
+package GeeksBasic;
+
+interface Animal{
+    void sound();  // Abstract class
+}
+class Dog implements Animal{
+    public void sound(){
+        System.out.println("Voooooo");
+    }
+}
+public class InterfaceExample {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.sound();
+    }
+}
