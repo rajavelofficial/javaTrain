c3a25ed5ea49da360dc6bddee5ec5e045022a4e1
rajavelofficial <rajavelofficial2003@gmail.com>
2025-10-05 08:04:13 +0530

    Char and String literals

--- GeeksBasic/LiteralInteger.java
+++ GeeksBasic/LiteralInteger.java
@@ -16,6 +16,30 @@ public class LiteralInteger {
         float a = 122.230f;
         float b = 0123.212f;
 
+        //Char Literals
+
+        char one = 'R';
+        char two = 061;
+        char unicode = '\u0035'; // The final output of the char is R15, Yamaha bike model
+
+        //String Literals
+
+        String text = "This is a String literal\n"
+
+            + "which spans not one and not two\n"
+
+            + "but three lines of text.\n";
+
+        String val = "Hello World!";
+        
+        System.out.println(val);
+        System.out.println(text);
+
+
+        System.out.print(one);
+        System.out.print(two);
+        System.out.println(unicode); 
+
         System.out.println(a);
         System.out.println(b);
 
