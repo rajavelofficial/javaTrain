b8907022db7b0283670e002640a7edbf1fe8077d
rajavelofficial <rajavelofficial2003@gmail.com>
2025-10-07 19:34:05 +0530

    Just practice the Integral Literal values

---
+++ GeeksBasic/Trail.java
@@ -0,0 +1,11 @@
+public class Trail {
+    public static void main(String[] args) {
+        
+        int a = 0xf + 9;          //HexaDecimal Literal
+        int b = 0b00010001;       //Binary Literal
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
