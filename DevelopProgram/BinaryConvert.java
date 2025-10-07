2784d002a6c35c1353cdf710990109a3a41b9c8f
rajavelofficial <rajavelofficial2003@gmail.com>
2025-10-07 21:27:26 +0530

    update all the data

---
+++ DevelopProgram/BinaryConvert.java
@@ -0,0 +1,22 @@
+package javaTrain.DevelopProgram;
+
+import java.util.Scanner;
+
+public class BinaryConvert {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+        System.out.println("Enter the number you want to convert as Binary Value : ");
+        int FirstValue = value.nextInt();
+
+        String second = "";
+        int First = FirstValue;
+
+        while( First > 0 ){
+            second = (First % 2) + second;
+            First = First / 2;
+        }
+        System.out.println(second);
+        
+    }
+    
+}
--- GeeksBasic/Trail.java
+++ GeeksBasic/Trail.java
@@ -2,7 +2,7 @@ public class Trail {
     public static void main(String[] args) {
         
         int a = 0xf + 9;          //HexaDecimal Literal
-        int b = 0b00010001;       //Binary Literal
+        int b = 0b101011;       //Binary Literal
         int Year = 0b11111010011; //2003 Binary Value
 
         float c = 12;             //Floating point Literal
