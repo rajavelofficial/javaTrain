dcae558281974e16a69b6c3ccd226876ff11da77
rajavelofficial <rajavelofficial2003@gmail.com>
2025-10-16 20:11:15 +0530

    Arithmetic Operators

---
+++ GeeksBasic/Arithmatic.java
@@ -0,0 +1,23 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        int Sum = a + b;
+
+        int Different = a - b;
+
+        int Multiple = a * b;
+
+        int Divition = b / a;
+
+        int modulo = a % b;
+
+        System.out.println("Addition of a & b Value : " + Sum);
+        System.out.println("Different of a & b Value : " + Different);
+        System.out.println("Multiple of a & b Value : " + Multiple);
+        System.out.println("Divition of a & b Value : " + Divition);
+        System.out.println("Mod value of a & b : " + modulo);
+        
+    }
+    
+}
