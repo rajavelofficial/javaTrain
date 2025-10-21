d3d61942d22d81c5ea112b68942fe293161e5262
rajavelofficial <rajavelofficial2003@gmail.com>
2025-10-22 01:34:01 +0530

    Testing the Unary Operand

---
+++ GeeksBasic/Unary.java
@@ -0,0 +1,16 @@
+public class Unary {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Increment Operand Operations : ");
+        System.out.println( "" + (a++));
+        System.out.println( (++a) + "\n" );
+
+        System.out.println("Decrement Operand Operations : ");
+        System.out.println( "" + (b--));
+        System.out.println( "" + (--b));
+
+    }
+    
+}
