8fe7c6e1498eed475cb03f85b6fa13473b094837
rajavelofficial <rajavelofficial2003@gmail.com>
2025-11-07 19:42:44 +0530

    just push all the files

---
+++ DecisionMaking/IfStatement.java
@@ -0,0 +1,13 @@
+public class IfStatement {
+    public static void main(String[] args){
+        int i = 10;
+
+        if ( i < 9){
+            System.out.println("Condition Execute here.");
+        }
+        if(i < 15){
+            System.out.println("The upper IF is false.So the Condition execute here.");
+        }
+    }
+    
+}
