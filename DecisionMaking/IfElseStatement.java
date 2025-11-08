678f30448cb734cf8caa273de6c76cdb45286177
rajavelofficial <rajavelofficial2003@gmail.com>
2025-11-08 17:43:29 +0530

    Modify the Condition value 9 after 15 Now check the true condition check

--- DecisionMaking/IfElseStatement.java
+++ DecisionMaking/IfElseStatement.java
@@ -2,7 +2,7 @@ public class IfElseStatement {
     public static void main(String[] args) {
         int x = 10;
 
-        if( x < 9 )
+        if( x < 15 )
         System.out.println("The Condition is true");
         else
         System.out.println("The Condition is wrong!");
