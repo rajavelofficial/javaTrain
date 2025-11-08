f97d4c0458f86883fa5f2f7a15e9bd4378b2b278
rajavelofficial <rajavelofficial2003@gmail.com>
2025-11-08 17:41:15 +0530

    Using the IF-Else Condition

---
+++ DecisionMaking/IfElseStatement.java
@@ -0,0 +1,11 @@
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if( x < 9 )
+        System.out.println("The Condition is true");
+        else
+        System.out.println("The Condition is wrong!");
+    }
+    
+}
