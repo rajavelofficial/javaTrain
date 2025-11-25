71e426455c8c65a18154f4fa00aa528a17deff50
rajavelofficial <rajavelofficial2003@gmail.com>
2025-11-26 01:06:41 +0530

    For Loop Demonstration is today task

--- DecisionMaking/IfStatement.java
+++ DecisionMaking/IfStatement.java
@@ -2,12 +2,11 @@ public class IfStatement {
     public static void main(String[] args){
         int i = 10;
 
-        if ( i < 9){
+        if ( i < 9)
             System.out.println("Condition Execute here.");
-        }
-        if(i < 15){
+        if(i < 15)
             System.out.println("The upper IF is false.So the Condition execute here.");
-        }
+        
     }
     
 }
