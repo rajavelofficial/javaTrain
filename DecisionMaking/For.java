363ce2ef311d2fe4300a7dfdb5d0b6c9a9afb643
rajavelofficial <rajavelofficial2003@gmail.com>
2025-12-01 11:36:42 +0530

    Print the star value revers

--- DecisionMaking/For.java
+++ DecisionMaking/For.java
@@ -1,6 +1,6 @@
 public class For {
     public static void main(String[] args) {
-        for(int i=0; i < 5 ; i++ ){
+        for(int i=4; i >=0 ; i-- ){
             for (int j = 0; j <= i; j++){
                 System.out.print("* ");
             }
