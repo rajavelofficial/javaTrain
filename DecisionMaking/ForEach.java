53ae6323eab926228859aa8bc4d67e0cdf6d4f7e
rajavelofficial <rajavelofficial2003@gmail.com>
2025-12-01 12:24:54 +0530

    using foreach loop condition to print the array values

---
+++ DecisionMaking/ForEach.java
@@ -0,0 +1,12 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] names = {"Tiger", "Lion", "Chita", "Elephant"};
+        System.out.println("Animals List: ");
+
+        for (String name : names) {
+            System.out.println(name);
+            
+        }
+    }
+    
+}
