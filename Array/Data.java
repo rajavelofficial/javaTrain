cef9da73b1e4e030289e8b9b1b41add0083babb1
rajavelofficial <rajavelofficial2003@gmail.com>
2026-02-03 16:42:05 +0530

    thining of array based addition values

--- Array/Data.java
+++ Array/Data.java
@@ -2,18 +2,18 @@ public class Data {
     public void primitive(){
         int[] arr = {10, 20, 30, 40};
         int n = arr.length;
+        int total = 0;
         
-        System.err.println("Primitive Type:");
-        System.out.println(" ");
+        System.err.print("Primitive Type: ");
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " + ");
+            total += arr[i];
         }
+        System.out.print(" = " +total);
         System.out.println("");
-        System.err.println("");
 
-        System.out.println("No Primitive Type:");
-        System.out.println("");
-        String[] name = {"Raja", "Ram", "Raju"};
+        System.out.print("No Primitive Type: ");
+        String[] name = {"Raja,", "Ram,", "Raju"};
         for (int j = 0; j < name.length; j++) {
             System.err.print(name[j] +" ");
             
