ecd1035c27d88463e4aea6c86d867ebc90a69e1a
rajavelofficial <rajavelofficial2003@gmail.com>
2026-05-10 20:48:36 +0530

    If-else Indentation example

---
+++ PythonTrain/Indentation.py
@@ -0,0 +1,11 @@
+#indentation using in If-else
+
+print('Age Checking')
+age = 20
+
+print('Your age was checking by CrackAI....')
+if age >= 18:
+    print('Your verified by CrackAI, Now you can use the System ')
+else:
+    print('Your not eligible for working with this System')
+print('All Set! Happy Coding!')
