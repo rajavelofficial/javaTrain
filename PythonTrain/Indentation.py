047e73380b5ec130bb9895a064dee97429b99fb0
rajavelofficial <rajavelofficial2003@gmail.com>
2026-05-11 17:31:05 +0530

    using indentation in loop

--- PythonTrain/Indentation.py
+++ PythonTrain/Indentation.py
@@ -1,5 +1,6 @@
 #indentation using in If-else
 
+'''
 print('Age Checking')
 age = 20
 
@@ -9,3 +10,23 @@ if age >= 18:
 else:
     print('Your not eligible for working with this System')
 print('All Set! Happy Coding!')
+
+'''
+
+#Indentation using Loop
+
+j = 1
+
+while(j <= 5):
+    print(j)
+    j += 1
+
+'''
+O/P:
+1
+2
+3
+4
+5
+
+'''
