f33a1f69bf216db83c6793d2139d60f9600226d8
rajavelofficial <rajavelofficial2003@gmail.com>
2026-05-13 14:30:33 +0530

    Learning about python comments

--- PythonTrain/comment.py
+++ PythonTrain/comment.py
@@ -1,7 +1,16 @@
 # I am single line comment
 
 """ Multi-line comment used
-print("Python Comments") """
+print("Python Comments") 
+
+Comments in Python are the lines in the code that are ignored by the interpreter during the execution of the program.
+
+It enhance the readability of the code.
+It can be used to identify functionality or structure the code-base.
+It can help understanding unusual or tricky scenarios handled by the code to prevent accidental removal or changes.
+It can be used to prevent executing any specific part of your code, while making changes or testing.
+
+"""
 
 # sample comment
 name = "\n.\n.\n.\n.\n. Cracks AI has now online "
