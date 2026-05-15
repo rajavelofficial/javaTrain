bdd8ae362e4862c2e89919607e856ded70e5e105
rajavelofficial <rajavelofficial2003@gmail.com>
2026-05-15 16:31:23 +0530

    prime number cheching program update

---
+++ DecisionMaking/samp2.java
@@ -0,0 +1,16 @@
+public class samp2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("First Number: " + a);
+
+        System.out.println("Second Number: " + b);
+
+        System.out.println("Sum: " + sum);
+
+    }
+}
