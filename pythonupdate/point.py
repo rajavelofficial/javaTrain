1243fe173acf49ae4a8431f702e79750eaa35d17
rajavelofficial <rajavelofficial2003@gmail.com>
2026-05-16 07:26:14 +0530

    Copyting two types of code

---
+++ pythonupdate/point.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive Number")
+
+elif num < 0:
+    print("Negative Number")
+
+else:
+    print("Zero")
+
+print("Python Program Example")
