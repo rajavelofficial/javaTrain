a819125441fc17b7f6b1baa0bd99261e5f27160f
rajavelofficial <rajavelofficial2003@gmail.com>
2026-05-29 09:49:09 +0530

    Just update the code

---
+++ PythonTrain/starprint.py
@@ -0,0 +1,15 @@
+class solution:
+
+    def printting(self, N):
+        print()
+
+        for i in range(N):
+            
+            for j in range(N):
+
+                print("*", end="")
+
+            print()
+sol = solution()
+N = 5
+sol.printting(N)
