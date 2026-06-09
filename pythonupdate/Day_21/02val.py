d0971a0c08da724dafeef97e4fee313809f4c0e8
rajavelofficial <rajavelofficial2003@gmail.com>
2026-06-09 11:33:35 +0530

    working in different string operations

---
+++ pythonupdate/Day_21/02val.py
@@ -0,0 +1,11 @@
+read = "Happy Coding CryptoRaja "
+
+#Printing function, print the value.
+print(read)
+
+#Checking the Data Type
+print(type(read))
+
+#Index based String value Printing
+print(read[0:5])
+print(read[-11:-1])
