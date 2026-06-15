18f60c67b2e64f67f7cf40f2bb31ad33e4739689
rajavelofficial <rajavelofficial2003@gmail.com>
2026-06-15 21:03:43 +0530

    Checking the truthy values of Non-Empty list values

---
+++ PythonTrain/truthy.py
@@ -0,0 +1,12 @@
+Number = 7
+
+if Number % 2:
+    print(Number ," is odd.")
+else:
+    print(Number ,"is Even")
+
+#Checking Truthy values
+
+a = [1, 2, 3]
+if a:
+    print(" The Non-Empty values of List are Called Truthy.")
