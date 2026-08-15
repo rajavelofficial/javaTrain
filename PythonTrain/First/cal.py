938a87e3f27388a7eaf5e6c4cf786b2418c32c1d
rajavelofficial <rajavelofficial2003@gmail.com>
2026-08-15 08:23:26 +0530

    Add Python learning and LeetCode code

---
+++ PythonTrain/First/cal.py
@@ -0,0 +1,6 @@
+total = 0
+num = int(input("Enter number (0 to stop): "))
+while num != 0:
+    total += num
+    num = int(input("Enter number (0 to stop): "))
+print("Total sum:", total)
--- PythonTrain/First/countdown.py
+++ PythonTrain/First/countdown.py
@@ -1,5 +1,6 @@
 seconds = int(input("Enter countdown time in seconds: "))
 
+delay = 1000
 while seconds > 0:
     print("Time left:", seconds)
     seconds -= 1
